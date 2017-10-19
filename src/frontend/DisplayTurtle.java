a66dade47e4fea9ccf0467e0b68b3a17ece9d8bc
Owen Smith <os29@duke.edu>
2017-10-18 21:37:28 -0400

    Added origin specification and path class, as well as DisplayTurtle class

--- src/frontend/DisplayTurtle.java
+++ src/frontend/DisplayTurtle.java
@@ -3,14 +3,19 @@ package frontend;
 import javafx.scene.image.ImageView;
 
 public class DisplayTurtle {
-	public static final double ORIGIN_X = 0;	//might change later if we change the configuration of the grid
-	public static final double ORIGIN_Y = 0;	//might change later if we change the configuration of the grid
 	private ImageView myTurtle;
 	
-	public DisplayTurtle(ImageView turtle) {
+	public DisplayTurtle(ImageView turtle, double originX, double originY) {
 		myTurtle = turtle;
-		myTurtle.setX(ORIGIN_X);
-		myTurtle.setY(ORIGIN_Y);
+		myTurtle.setX(originX);
+		myTurtle.setY(originY);
+	}
+	
+	protected void updateTurtle(Turtle turtle) {
+		myTurtle.setX(turtle.getX());
+		myTurtle.setY(turtle.getY());
+		myTurtle.setRotattion(turtle.getRotation());
+		myTurtle.setVisible(turtle.isVisible());
 	}
 
 }
---
+++ src/frontend/TurtlePath.java
@@ -0,0 +1,35 @@
+package frontend;
+
+import java.util.List;
+
+import javafx.scene.shape.LineTo;
+import javafx.scene.shape.MoveTo;
+import javafx.scene.shape.Path;
+
+
+public class TurtlePath {
+	public static final int X_INDEX = 0;
+	public static final int Y_INDEX = 1;
+	private Path path;
+	private int prevPathSize;
+	
+	public TurtlePath(double origin_x,double origin_y) {
+		path = new Path();
+		prevPathSize = 0;
+		path.getElements().add(new MoveTo(origin_x,origin_y));
+	}
+	
+	protected void updatePath(List<List<Double>> start,List<List<Double>> end) {
+		int currPathSize = start.size();
+		if(currPathSize > prevPathSize) {
+			for(int i = 0; i < start.size(); i++) {
+				double startX = start.get(i).get(X_INDEX);
+				double startY = start.get(i).get(Y_INDEX);
+				double endX = end.get(i).get(X_INDEX);
+				double endY = end.get(i).get(Y_INDEX);
+				path.getElements().add(new MoveTo(startX,startY));
+				path.getElements().add(new LineTo(endX,endY));
+			}
+		}
+	}
+}
