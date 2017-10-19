1bb66538be09fbad0bbe45c93842fc364e64f486
Yiqin Zhou <yz259@duke.edu>
2017-10-18 22:32:55 -0400

    Fixed merge issue

--- src/frontend/DisplayTurtle.java
+++ src/frontend/DisplayTurtle.java
@@ -3,16 +3,6 @@ package frontend;
 import javafx.scene.image.ImageView;
 
 public class DisplayTurtle {
-
-	public static final double ORIGIN_X = 0;	//might change later if we change the configuration of the grid
-	public static final double ORIGIN_Y = 0;	//might change later if we change the configuration of the grid
-	private ImageView myTurtle;
-	
-	public DisplayTurtle(ImageView turtle) {
-		myTurtle = turtle;
-		myTurtle.setX(ORIGIN_X);
-		myTurtle.setY(ORIGIN_Y);
-
 	private ImageView myTurtle;
 	
 	public DisplayTurtle(ImageView turtle, double originX, double originY) {
@@ -26,7 +16,6 @@ public class DisplayTurtle {
 		myTurtle.setY(turtle.getY());
 		myTurtle.setRotattion(turtle.getRotation());
 		myTurtle.setVisible(turtle.isVisible());
-
 	}
 
 }
