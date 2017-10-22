49edaea2f3374978ed0d8e2629c5dc02be43c832
Kelly Zhang <k.zhang@duke.edu>
2017-10-21 20:59:16 -0400

    Update Back.java

--- src/backend/commands/turtleCommands/Back.java
+++ src/backend/commands/turtleCommands/Back.java
@@ -1,5 +1,19 @@
 package backend.commands.turtleCommands;
 
-public class back {
+import java.util.Stack;
 
+import backend.Turtle;
+import backend.commands.Command;
+
+/**
+ * @author kelly
+ *
+ */
+public class Back implements Command {
+	
+	@Override
+	public Stack<Double> execute(Stack<Double> vars, Turtle currentTurtle) {
+		currentTurtle.move(-1*vars.peek());
+		return vars; // new Stack
+	}
 }
