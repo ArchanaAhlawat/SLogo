f45a2c51f9afcc2ebd43e0a067314dca29b453ac
Kelly Zhang <kz49@duke.edu>
2017-10-28 00:01:08 -0400

    fixed not command

--- src/backend/commands/booleanCommands/Not.java
+++ src/backend/commands/booleanCommands/Not.java
@@ -5,6 +5,7 @@ import java.util.Stack;
 import backend.Stacks;
 import backend.Turtle;
 import backend.api.Command;
+import backend.commands.BooleanOperation;
 
 /**
  * has the execute for when a not command is called
@@ -15,13 +16,10 @@ import backend.api.Command;
  * @author kelly
  *
  */
-public class Not implements Command {
+public class Not extends BooleanOperation {
 
-	private static final double ZERO = 0.0;
-	private static final double ONE = 1.0;
-	
-	
 	public Not(Stacks myStack, Turtle myTurtle) {
+		super(myStack, myTurtle);
 	}
 
 	@Override
