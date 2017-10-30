f2fb952423028027e32d5231d85a31a280f98aff
Kelly Zhang <kz49@duke.edu>
2017-10-30 10:17:50 -0400

    setpencolor implemented

--- src/backend/commands/displayCommands/SetPenColor.java
+++ src/backend/commands/displayCommands/SetPenColor.java
@@ -1,5 +1,17 @@
 package backend.commands.displayCommands;
 
-public class SetPenColor {
+import backend.Stacks;
+import backend.TurtleTree;
 
+public class SetPenColor extends DisplayCommands {
+
+	public SetPenColor(Stacks myStack, TurtleTree myTurtle) {
+		super(myStack, myTurtle);
+	}
+
+	@Override
+	public void execute(Stacks instructionStacks, TurtleTree currentTurtle) {
+		double expr1 = instructionStacks.peekDouble();
+		instructionStacks.addDouble(currentTurtle.setPenColor((int) expr1));
+	}
 }
