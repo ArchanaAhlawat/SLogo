c5fa785d59b2132f468793a02fb35529b10c9f28
Kelly Zhang <kz49@duke.edu>
2017-10-30 23:16:41 -0400

    pls get rid of this untracked file

---
+++ src/backend/api/TurtleManager.java
@@ -0,0 +1,11 @@
+package backend.api;
+
+import backend.TurtleTree;
+
+public class TurtleManager {
+	public TurtleTree badTurtle;
+
+	public TurtleManager(TurtleTree badTurtle) {
+		this.badTurtle = badTurtle;
+	}
+}
